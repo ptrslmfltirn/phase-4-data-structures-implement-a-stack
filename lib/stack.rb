6770b86084af01bda065a7c5ce27c2103dfd8f43
Peter Lamson <ptrlmsn@gmail.com>
2022-02-21 10:30:22 -0500

    commit tree cc6ac8b52b3b2fb1be58e5885f9de0a1367e3805

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,14 @@
-# your code here
+class Stack 
+    def initialize 
+        @stack = [] 
+    end 
+    def push(v) 
+        @stack.push(v)
+    end 
+    def pop
+        @stack.pop
+    end 
+    def peek 
+        @stack.last 
+    end 
+end 
